b83fa7cff7e8f766e3f78323e8e87784378c754c
Alex Clark <aclark@aclark.net>
2022-05-11 09:01:23 -0400

    A one time black event

--- project/settings/dev.py
+++ project/settings/dev.py
@@ -4,12 +4,12 @@ from .base import *
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e08s038-692*py^*%(0k!t19)l(=-x&%*xq$hp#u189r06%3o2'
+SECRET_KEY = "django-insecure-e08s038-692*py^*%(0k!t19)l(=-x&%*xq$hp#u189r06%3o2"
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = ["*"]
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 
 try:
