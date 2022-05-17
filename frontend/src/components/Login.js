3c632fb5cef9c79e2c0fb7d0beebab74ebfa6d19
Alex Clark <aclark@aclark.net>
2022-05-17 10:48:28 -0400

    Update

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -8,42 +8,6 @@ const Login = ({csrfToken}) => {
     const [errorMessages, setErrorMessages] = useState({});
     const [isSubmitted, setIsSubmitted] = useState(false);
 
-    // User Login info
-    const database = [
-      {
-        username: "admin",
-        password: "admin"
-      }
-    ];
-
-    const errors = {
-      uname: "invalid username",
-      pass: "invalid password"
-    };
-
-    // const handleSubmit = (event) => {
-    //   //Prevent page reload
-    //   event.preventDefault();
-
-    //   var { uname, pass } = document.forms[0];
-
-    //   // Find user login info
-    //   const userData = database.find((user) => user.username === uname.value);
-
-    //   // Compare user info
-    //   if (userData) {
-    //     if (userData.password !== pass.value) {
-    //       // Invalid password
-    //       setErrorMessages({ name: "pass", message: errors.pass });
-    //     } else {
-    //       setIsSubmitted(true);
-    //     }
-    //   } else {
-    //     // Username not found
-    //     setErrorMessages({ name: "uname", message: errors.uname });
-    //   }
-    // };
-
     const onSubmit = (event) => {
       //Prevent page reload
       // event.preventDefault();
